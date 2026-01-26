bddbd353addf97c465d36c821160e941d3212de7
daw <driballoalvarez@safareyes.es>
2026-01-26 20:30:23 +0100

    inicio de secion, registro, seguridad, pagina admin

---
+++ src/Controller/AdminPeliculaController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Pelicula;
+use App\Form\PeliculaType;
+use App\Repository\PeliculaRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/pelicula')]
+final class AdminPeliculaController extends AbstractController
+{
+    #[Route(name: 'app_admin_pelicula_index', methods: ['GET'])]
+    public function index(PeliculaRepository $peliculaRepository): Response
+    {
+        return $this->render('admin_pelicula/index.html.twig', [
+            'peliculas' => $peliculaRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_pelicula_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $pelicula = new Pelicula();
+        $form = $this->createForm(PeliculaType::class, $pelicula);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($pelicula);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_pelicula_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin_pelicula/new.html.twig', [
+            'pelicula' => $pelicula,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_pelicula_show', methods: ['GET'])]
+    public function show(Pelicula $pelicula): Response
+    {
+        return $this->render('admin_pelicula/show.html.twig', [
+            'pelicula' => $pelicula,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_pelicula_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Pelicula $pelicula, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PeliculaType::class, $pelicula);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_pelicula_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin_pelicula/edit.html.twig', [
+            'pelicula' => $pelicula,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_pelicula_delete', methods: ['POST'])]
+    public function delete(Request $request, Pelicula $pelicula, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$pelicula->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($pelicula);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_admin_pelicula_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/PeliculaType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Pelicula;
+use App\Entity\Ranking;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PeliculaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('apiId')
+            ->add('titulo')
+            ->add('description')
+            ->add('year')
+            ->add('image_url')
+            ->add('genre')
+            ->add('stars')
+            ->add('rankings', EntityType::class, [
+                'class' => Ranking::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Pelicula::class,
+        ]);
+    }
+}
